27a0bb6f47b9df6f5c3ab2644c4ca58d7923bcf9
Philippe Lafontaine <lafontaine.philippe@gmail.com>
2018-11-04 09:55:36 -0500

    feat(eslint-config): add prettier config and add leniency on tests files
    affects: @bleushan/eslint-config

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,9 +1,5 @@
 module.exports = {
-  extends: [
-    'standard',
-    'cleanjs',
-    'plugin:jest/recommended'
-  ],
+  extends: ['standard', 'cleanjs', 'plugin:jest/recommended', 'prettier'],
   env: {
     browser: true,
     serviceworker: true,
@@ -11,12 +7,31 @@ module.exports = {
   },
   parser: 'babel-eslint',
   rules: {
-    'arrow-parens': ["error", "as-needed", { "requireForBlockBody": true }],
-    'space-before-function-paren': ['error', {
-        'anonymous': 'always',
-        'named': 'never',
-        'asyncArrow': 'always'
-    }],
+    'arrow-parens': ['error', 'as-needed', { requireForBlockBody: true }],
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always'
+      }
+    ],
     'fp/no-rest-parameters': 'off'
-  }
+  },
+  overrides: [
+    {
+      files: [
+        '*-test.js',
+        '*.spec.js',
+        '*.test.js',
+        '**/__tests__/**/*.js',
+        '**/tests/**/*.js'
+      ],
+      rules: {
+        'fp/no-unused-expression': 'off',
+        'fp/no-nil': 'off',
+        'better/explicit-return': 'off'
+      }
+    }
+  ]
 }
